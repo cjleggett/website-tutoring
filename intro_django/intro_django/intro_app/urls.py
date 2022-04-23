33e4888737cd324a856172f0e716ba33318b0cc4
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2022-04-23 10:19:45 -0400

    4/23

--- intro_django/intro_django/intro_app/urls.py
+++ intro_django/intro_django/intro_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("todo", views.todo, name="todo"),
+    path("add_task", views.add_task, name="add_task"),
     path("<str:name>", views.sayhello, name="sayhello")
 ]
--- intro_django/intro_django/intro_app/views.py
+++ intro_django/intro_django/intro_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+items = [
+    "Do Laundry",
+    "Do homework",
+    "Sweep floor"
+]
+
+class AddTaskForm(forms.Form):
+    new_task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -9,3 +20,20 @@ def sayhello(request, name):
     return render(request, "intro_app/sayhello.html", {
         "name": name
     })
+
+def todo(request):
+    return render(request, "intro_app/todo.html", {
+        "items": items,
+        "form": AddTaskForm()
+    })
+
+def add_task(request):
+    # Add task to list
+    form = AddTaskForm(request.POST)
+
+    if form.is_valid():
+        task = form.cleaned_data["new_task"]
+        items.append(task)
+
+
+    return HttpResponseRedirect(reverse("todo"))
